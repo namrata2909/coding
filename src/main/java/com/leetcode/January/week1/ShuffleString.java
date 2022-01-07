2970f8ec6d26ba1a5a7a29788d77317f0ae1bf30
namrata <namrata1061995@gmail.com>
2022-01-07 23:32:03 +0530

    Code for ShuffleString

---
+++ src/main/java/com/leetcode/January/week1/ShuffleString.java
@@ -0,0 +1,23 @@
+package com.leetcode.January.week1;
+
+public class ShuffleString {
+    public String restoreString(String s, int[] indices) {
+        if (s == null || indices == null) {
+            return null;
+        }
+        /*
+        Wrong: first index pe kya rakhna hai
+        String shuffleString = "";
+        for (int i : indices) {
+            shuffleString += s.charAt(i);
+        }
+        return shuffleString;
+        */
+        // c ko kis index pe rakhna hai -> correct
+        char[] ans = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            ans[indices[i]] = s.charAt(i);
+        }
+        return new String(ans);
+    }
+}
---
+++ src/test/java/com/leetcode/january/week1/ShuffleStringTest.java
@@ -0,0 +1,30 @@
+package com.leetcode.january.week1;
+
+import com.leetcode.January.week1.ShuffleString;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class ShuffleStringTest {
+
+    private final ShuffleString shuffleString = new ShuffleString();
+
+    private static Stream<Arguments> getArguments() {
+        return Stream.of(
+                Arguments.of("codeleet", new int[]{4, 5, 6, 7, 0, 2, 1, 3}, "leetcode"),
+                Arguments.of("abc", new int[]{0, 1, 2}, "abc"),
+                Arguments.of(null, null, null)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "getArguments")
+    public void executeTestCase(String s, int[] indices, String ans) {
+        Assertions.assertEquals(ans, shuffleString.restoreString(s, indices));
+    }
+
+}
