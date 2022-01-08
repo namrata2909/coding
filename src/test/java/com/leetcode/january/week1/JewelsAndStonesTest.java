96d3dded37e96e3ffd03ad04555e8657a653dc08
namrata <namrata1061995@gmail.com>
2022-01-08 14:36:29 +0530

    reformat code

--- src/test/java/com/leetcode/january/week1/JewelsAndStonesTest.java
+++ src/test/java/com/leetcode/january/week1/JewelsAndStonesTest.java
@@ -5,19 +5,19 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class JewelsAndStonesTest {
+ class JewelsAndStonesTest {
 
-    private final JewelsAndStones jewelsAndStones = new JewelsAndStones();
+     private final JewelsAndStones jewelsAndStones = new JewelsAndStones();
 
-    @ParameterizedTest
-    @CsvSource(value = {
-            "aA:aAAbbbb:3",
-            "z:ZZ:0",
-            "::0",
-            ":asdass:0",
-    }, delimiter = ':')
-    public void executeTestCase(String jewels, String stones, int ans) {
-        Assertions.assertEquals(ans, jewelsAndStones.numJewelsInStones(jewels, stones));
-    }
+     @ParameterizedTest
+     @CsvSource(value = {
+             "aA:aAAbbbb:3",
+             "z:ZZ:0",
+             "::0",
+             ":asdass:0",
+     }, delimiter = ':')
+     void executeTestCase(String jewels, String stones, int ans) {
+         Assertions.assertEquals(ans, jewelsAndStones.numJewelsInStones(jewels, stones));
+     }
 
 }
